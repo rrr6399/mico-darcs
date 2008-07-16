2a2728a6913afabe9c53d0f8326cb91406b1e96e
Karel Gardas <kgardas@objectsecurity.com>
2008-07-16 04:13:13 -0500

    add admin/build_with_vc.bat batch to support Windows-based buildbot slaves
    darcs-hash:8a3c2aa347c61f08d916d25237f15ae262b62d53

---
+++ admin/build_with_vc.bat
@@ -0,0 +1,12 @@
+@echo off
+@rem
+@rem This script is intended to be used to build MICO from
+@rem environments where it is not possible to invoke
+@rem Visual Studio command-line directly (i.e. buildbot)
+@rem
+@rem The first parameters is a batch file which sets all
+@rem appropriate environment variables (i.e. vsvars32.bat)
+@rem Other parameters are then sent to nmake directly
+@rem
+call %1
+nmake /f Makefile.win32 %2 %3 %4 %5 %6 %7
