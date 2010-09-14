1058b4d68cb2f77716cb49a5d55228bd7121e869
Karel Gardas <kgardas@objectsecurity.com>
2010-09-14 03:03:00 -0500

    fix DefaultValueRefCountBase dead-lock in obv/tricky demo by changing mutex to recursive
    darcs-hash:8f5916dabd9d936f31f5cf63125eb05aca6a39fb

--- orb/value.cc
+++ orb/value.cc
@@ -1,6 +1,6 @@
 /*
  *  MICO --- an Open Source CORBA implementation
- *  Copyright (c) 1997-2001 by The Mico Team
+ *  Copyright (c) 1997-2010 by The Mico Team
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -47,6 +47,7 @@ using namespace std;
 
 
 CORBA::DefaultValueRefCountBase::DefaultValueRefCountBase ()
+    : lock_(FALSE, MICOMT::Mutex::Recursive)
 {
 }
 
